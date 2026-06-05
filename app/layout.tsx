d9b265f71834211f9f55672e79d0d33907ca4745
ota <ota.kazuyuki.s6@dc.tohoku.ac.jp>
2026-06-05 11:47:51 +0900

    画面構成の修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,13 @@
-import GlobalNav from "@/components/GlobalNav";
 import "./globals.css";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
-}){
+}) {
   return (
     <html lang="ja">
-      <body className="bg-slate-950 text-slate-50 min-h-screen">
-        <GlobalNav />
-        <main className="pt-0 pb-16 md:pt-16 md:pb-0 min-h-screen">{children}</main>
-      </body>
+      <body className="bg-slate-950 text-slate-50 min-h-screen">{children}</body>
     </html>
-  )
-}
+  );
+};
